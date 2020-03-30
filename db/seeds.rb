4359b7e541eabbf910821bdfb44d1ccfcc208f05
galta95 <galtaebi@gmail.com>
2020-03-30 18:20:21 +0300

    Implemented show functionality for portfolio module

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ puts "5 skills created"
         subtitle: "My great service",
         body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
         main_image: "http://placehold.it/600x200",
-        thumb_image: "http://placehold.it/350x200"
+        thumb_image: "http://placehold.it/350x200",
     )
 end
 
